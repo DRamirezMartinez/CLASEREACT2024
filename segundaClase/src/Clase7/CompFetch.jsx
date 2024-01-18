a98f9c955e569aa5332a89f2ae9e0c876ab11601
Diego Ramirez <ram_die@hotmail.com>
2024-01-18 16:31:15 -0500

    ejemplo2Fectch tienda api externa

--- segundaClase/src/Clase7/CompFetch.jsx
+++ segundaClase/src/Clase7/CompFetch.jsx
@@ -1,27 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CompFetch = () =>{
 
     const [productos, setProductos] = useState ([]);
 
+
+   useEffect(() => { 
     fetch("https://fakestoreapi.com/products/")
     .then(respuesta => respuesta.json())
     .then(datos => {
-        console.log(datos);
+        setProductos(datos);
     });
+
+    })
     return (
         <div className="container">
             <div className="row">
-               <div className="col">
-               <h1>Fetch</h1>
-        <ul>
-            {productos.map(produc =>(
-                <li> {produc.title} - ${produc.price} ({produc.category}) </li>
-            ))}
-        </ul> 
-               </div> 
-            </div>
-      
+               <div className="col text-center">
+                    <h1>Fake Store API</h1>
+               </div>
+            </div> 
+
+            <div className="row">
+                {productos.map(produc =>(
+                    <div key={produc.id} className="col-md-3 ">
+                        <div className="card">
+                            <img src= {produc.image} className="img-fluid" alt={produc.title}/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title {produc.title} </h5>
+                                <p className="card-text"> ${produc.price} </p>                                
+                            </div>
+                        </div>
+                        </div>                        
+                        ))}
+                   
+            </div>                                
         </div>
        
     )
